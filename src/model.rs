aaadb76fd003ee1c6bd2c463da4256e2f134c9af
Dante Dam <laggycomputer@yahoo.com>
2025-02-05 11:35:06 -0800

    model update

--- src/model.rs
+++ src/model.rs
@@ -16,4 +16,18 @@ pub struct Message {
 pub struct Snipe {
     pub message_id: i64,
     pub victim_id: i64,
+    pub long_lat: (f64, f64),
+    pub notes: String,
+}
+
+impl Snipe {
+    #[inline]
+    fn lat(&self) -> f64 {
+        self.long_lat.1
+    }
+
+    #[inline]
+    fn long(&self) -> f64 {
+        self.long_lat.0
+    }
 }
