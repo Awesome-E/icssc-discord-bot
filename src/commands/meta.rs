e8086b769c4df9516291b9ebfd4bb392108f0b8a
Dante Dam <laggycomputer@yahoo.com>
2025-02-04 23:13:21 -0800

    ping checks sql

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,14 +1,24 @@
 use crate::{BotError, Context};
+use diesel::Connection;
 
 /// Check bot is alive, get numerical ping to Discord
 #[poise::command(prefix_command, slash_command)]
 pub(crate) async fn ping(ctx: Context<'_>) -> Result<(), BotError> {
     let ping_num = ctx.ping().await.as_millis();
 
-    ctx.say(match ping_num {
-        0 => String::from("ok, waiting for more data to report ping"),
-        _ => format!("hi, heartbeat is pinging in {} ms", ping_num),
-    })
+    let conn = ctx.data().db_conn();
+
+    ctx.say(format!(
+        "{}\n\n{}",
+        match ping_num {
+            0 => String::from("ok, waiting for more data to report ping"),
+            _ => format!("hi, heartbeat is pinging in {} ms", ping_num),
+        },
+        match conn.map(|mut conn| conn.begin_test_transaction()) {
+            Ok(_) => String::from("postgres ok"),
+            Err(err) => format!("postgres not ok: {}", err),
+        }
+    ))
     .await?;
     Ok(())
 }
