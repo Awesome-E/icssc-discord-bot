046d46539ea811ceba3718a81f88c1b429e9fe70
Dante Dam <laggycomputer@yahoo.com>
2025-02-05 11:28:11 -0800

    new data

--- src/schema.rs
+++ src/schema.rs
@@ -1,5 +1,11 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "point", schema = "pg_catalog"))]
+    pub struct Point;
+}
+
 diesel::table! {
     message (message_id) {
         guild_id -> Int8,
@@ -10,9 +16,14 @@ diesel::table! {
 }
 
 diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::Point;
+
     snipe (message_id, victim_id) {
         message_id -> Int8,
         victim_id -> Int8,
+        location -> Nullable<Point>,
+        notes -> Nullable<Text>,
     }
 }
 
