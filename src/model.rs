28c2e6669173c76ad20e94a55706bc02150281a4
Dante Dam <laggycomputer@yahoo.com>
2025-02-04 23:07:20 -0800

    model

---
+++ src/model.rs
@@ -0,0 +1,19 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::message)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Message {
+    guild_id: i64,
+    channel_id: i64,
+    message_id: i64,
+    author_id: i64,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::snipe)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Snipe {
+    pub message_id: i64,
+    pub victim: i64,
+}
